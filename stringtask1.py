c0ad1858d5ddb672a7d3bc1e1e4e4b3e3bb47b62
jnjijij <136825012+jnjijij@users.noreply.github.com>
2024-01-04 18:27:53 +0200

    Update stringtask1.py

--- stringtask1.py
+++ stringtask1.py
@@ -1,6 +1,18 @@
-import re
-
 st = 'as 23 fdfdg544'
-numbers = re.findall(r'\d+', st)
+
+numbers = []
+current_number = ''
+
+for char in st:
+    if char.isdigit():
+        current_number += char
+    elif current_number:
+        numbers.append(current_number)
+        current_number = ''
+
+
+if current_number:
+    numbers.append(current_number)
+
 result = ','.join(numbers)
 print(result)
